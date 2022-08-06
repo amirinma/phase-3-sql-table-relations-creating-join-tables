6f86bd42adc334e67901fb184ba7e4b5a8cfa5f3
Mohammad <zia.ro@live.com>
2022-08-06 17:36:45 -0400

    Completed Assingment

---
+++ 01_Query_JOIN_TABLE.sql
@@ -0,0 +1,24 @@
+.header on 
+.mode column 
+.width auto 
+SELECT owners.name 
+FROM owners
+INNER JOIN cat_owners
+ON owners.id = cat_owners.owner_id 
+WHERE cat_owners.cat_id = 3 ;
+
+
+SELECT cats.name
+FROM cats 
+INNER JOIN cat_owners
+ON cats.id = cat_owners.cat_id
+WHERE cat_owners.owner_id = 2;
+
+
+SELECT cats.name AS cat_name, 
+owners.name AS owner_name
+FROM cats 
+INNER JOIN cat_owners
+ON cats.id = cat_owners.cat_id
+INNER JOIN owners
+ON cat_owners.owner_id = owners.id;
